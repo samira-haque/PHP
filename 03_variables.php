80b7c4b21a5ab3acad711af6eb6ec853b8fb24dd
samira haque <sh6007530@gmail.com>
2025-10-02 20:13:42 +0600

    modified php

---
+++ 03_variables.php
@@ -0,0 +1,13 @@
+<?php
+// echo " This is Variable" ;  // not case sensitive
+// variables are containers for storing information
+// starts with a $
+$name = "Samira" ;
+$income = 000 ;
+
+echo "This girl is $name and her income is $income <br>" ;
+
+echo "$name is a good girl <br>";
+echo "$name is a good girl ... <br>";
+
+?>

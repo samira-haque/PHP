027f5198d594bd79a99d214aabaf6e0469fcc152
samira haque <sh6007530@gmail.com>
2025-10-06 21:36:51 +0600

    php

---
+++ 08_ifelse.php
@@ -0,0 +1,43 @@
+<?php
+// $a = 65;
+// $b = 78;
+
+// if($a > 78){
+//     echo "a is greater than 78";
+// }
+// else{
+//     echo "a is no greater than 78";
+// }
+
+// $age = 4;
+
+// if($age > 18){
+//     echo "you can drive the car " ;
+// }
+
+// elseif($age > 13)
+// {
+//     echo " you cannt drive";
+// }
+
+// else{
+//     echo " you can go with your parents";
+// }
+
+
+$age = 4;
+
+if($age > 18){
+    echo "you can drive the car " ;
+}
+
+if($age > 13)
+{
+    echo " you cannt drive";
+}
+
+else{
+    echo " you can go with your parents";
+}
+
+?>
